c329d69c2bb497411c716ecf9f3a5d6de808c979
xiomaratoledo <138639501+xiomaratoledo@users.noreply.github.com>
2024-03-12 17:27:20 -0700

    Update ReverseNumber.java

--- Basics/ReverseNumber.java
+++ Basics/ReverseNumber.java
@@ -13,10 +13,24 @@ public class ReverseNumber {
     public static int reverse(int number) {
         int reversedNumber = 0;
         while (number != 0) {
-            int digit = number % 10;
-            reversedNumber = reversedNumber * 10 + digit;
-            number /= 10;
+            int digit = number % 10; // a. Extract the last digit of the input number
+            reversedNumber = reversedNumber * 10 + digit; // b. Append the extracted digit to the reversedNumber
+            number /= 10; // c. Remove the last digit from the input number
         }
         return reversedNumber;
     }
 }
+
+/*
+1. Start
+2. Input the number to be reversed
+3. Initialize reversedNumber to 0
+4. While the input number is not equal to 0:
+     a. Extract the last digit of the input number
+     b. Append the extracted digit to the reversedNumber
+     c. Remove the last digit from the input number
+5. End loop
+6. Output the reversedNumber
+7. End
+*/
+
